57a635c5e55a24561223ac42d4b7e8cde209d63e
Alexander Kalis <alex.kalis@oulook.com>
2021-08-05 21:22:30 +0200

    update

--- AutoMaintenance/MainWindow.xaml.cs
+++ AutoMaintenance/MainWindow.xaml.cs
@@ -1,19 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Win32;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Microsoft.Win32;
+using System.Diagnostics;
 using System.IO;
+using System.IO.Compression;
 
 namespace AutoMaintenance
 {
@@ -22,6 +12,8 @@ namespace AutoMaintenance
     /// </summary>
     public partial class MainWindow : Window
     {
+        string[] filePath = new string[100];
+        int zipCounter = 0;
         public MainWindow()
         {
             InitializeComponent();
@@ -43,14 +35,18 @@ namespace AutoMaintenance
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
             openFileDialog.Multiselect = true;
-            openFileDialog.Filter = "XML Files (*.xml)|*.xml";
+            openFileDialog.Filter = "ZIP Files (*.zip)|*.zip";
+            
 
             if (openFileDialog.ShowDialog() == true)
             {
-                foreach (var item in openFileDialog.FileNames)
+                zipCounter = 0;
+                foreach (string item in openFileDialog.FileNames)
                 {
-                    
-                    fileList.Items.Add(System.IO.Path.GetFileName(item));
+                    _ = fileList.Items.Add(System.IO.Path.GetFileName(item));
+                    filePath[zipCounter] = System.IO.Path.GetFullPath(item);
+                    //Trace.WriteLine("added " + filePath[zipCounter]);
+                    zipCounter++;
                 }
             }
 
@@ -59,6 +55,37 @@ namespace AutoMaintenance
         private void Clear_OnMouseDown(object sender, MouseButtonEventArgs e)
         {
             fileList.Items.Clear();
+            zipCounter = 0;
         }
+
+        private void Start_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (zipCounter > 0)
+            {
+                for (int i = 0; i < zipCounter; i++)
+                {
+                    //Trace.WriteLine(filePath[i]);
+                    using (ZipArchive archive = ZipFile.OpenRead(filePath[i]))
+                    {
+                        ZipArchiveEntry entry = archive.GetEntry("am.ini");
+                        if (entry != null)
+                        {
+                            //Trace.WriteLine("Data found");
+                            string tempFile = Path.GetTempFileName();
+                            entry.ExtractToFile(tempFile, true);
+                            string content = File.ReadAllText(tempFile);
+                            //Trace.Write(content);
+
+                        }
+
+                    }
+                }
+            }
+           
+        }
+
+
+
+
     }
 }
