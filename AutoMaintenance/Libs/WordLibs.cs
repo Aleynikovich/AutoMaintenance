e5e2652219d376c086b5a5b5cb614e3963f3e15d
Alexander Kalis <alex.kalis@outlook.com>
2022-06-20 21:05:25 +0200

    alfa 2.0.2

--- AutoMaintenance/Libs/WordLibs.cs
+++ AutoMaintenance/Libs/WordLibs.cs
@@ -63,6 +63,7 @@ namespace AutoMaintenance
                     FindAndReplace(wordApp, "<KrName>", krc.Name);
                     FindAndReplace(wordApp, "<KRCVersion>", krc.Version);
                     FindAndReplace(wordApp, "<KRCTechpacks>", krc.Tech);
+                    FindAndReplace(wordApp, "<LoadData>", krc.LoadData);
                     FindAndReplace(wordApp, "<MaintenanceDate>", System.DateTime.Now.Date);
                 }
 
